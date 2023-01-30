e47c74bf4950d96348c3f91abd1481ddb4cdaa30
Seokmin <newacon@naver.com>
2023-01-30 21:54:16 +0200

    first commit

---
+++ src/pages/blog.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Seo from "../components/Seo";
+
+function Blog() {
+  return (
+    <Layout title="Blog">
+      <p>The most recent news from my shop.</p>
+    </Layout>
+  );
+}
+
+export const Head = () => <Seo title="Blog" />;
+
+export default Blog;
